9233c391a7219ed9b9875601030c7ebadfe8980f
Nikhilk2002 <nikhilmkd1523@gmail.com>
2024-07-11 12:19:14 +0530

    Function composition

---
+++ FunctionComposition.js
@@ -0,0 +1,17 @@
+var compose = function (functions) {
+    return function (x) {
+        return functions.reduceRight((acc, fn) => fn(acc), x);
+    };
+};
+
+const functions1 = [x => x + 1, x => x * x, x => 2 * x];
+const composedFunction1 = compose(functions1);
+console.log(composedFunction1(4));
+
+const functions2 = [x => 10 * x, x => 10 * x, x => 10 * x];
+const composedFunction2 = compose(functions2);
+console.log(composedFunction2(1));
+
+const functions3 = [];
+const composedFunction3 = compose(functions3);
+console.log(composedFunction3(42));
